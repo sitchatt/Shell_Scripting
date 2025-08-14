e978df7ead8edd2d15d0144f1c99fbb4ea6492bb
Sitabja Chatterjee <89836369+sitchatt@users.noreply.github.com>
2025-08-14 18:10:47 +0530

    Update add-local-user.sh

--- add-local-user.sh
+++ add-local-user.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
-#
+
 ## Check User is Root or not
-#
-#
-set -x
-if [[ ${UID} != 0 ]];
-	echo "Not a ROOT user. Exiting the run"
-	exit 1
+
+if [[ ${UID} != 0 ]]; then
+    echo "Unauthorized !! Not a ROOT user. Exiting the Script"
+    exit 1
 fi
 
-#Prompt to enter user detail
+# Prompt to enter user details
 read -p 'Please Enter Your Name: ' NAME
 read -p 'Please Enter UserName: ' USERNAME
 read -p 'Please Enter Initial Password: ' PASSWORD
- #User Creation
-useradd -c ${NAME} -m ${USERNAME}
 
-if [[ $? -nq 0 ]]
-	echo "Unable to add user for ${USERNAME}. Exiting Script"
-	exit 1
+# User Creation
+useradd -c "${NAME}" -m "${USERNAME}"
+
+if [[ $? -ne 0 ]]; then
+    echo "Unable to add user for ${USERNAME}. Exiting Script"
+    exit 1
 else
-	echo " User is added. Creating Password....."
+    echo "User is added. Creating Password....."
 fi
 
-#Create Password and Expire that to force user to change password on next login
-echo $PASSWORD | passwd --stdin $USERNAME
-passwd -e $USERNAME
+# Create Password and Expire it to force user to change password on next login
+echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
+passwd -e "${USERNAME}"
 
-if [[ $? -nq 0 ]]
-        echo "Unable Create Password. Exiting Script"
-	exit 1
+if [[ $? -ne 0 ]]; then
+    echo "Unable to create password. Exiting Script"
+    exit 1
 fi
 
-echo " Successfully Created User "
-echo " User Name : ${USERNAME}"
-echo " Initial Password : ${PASSWORD}"
-echo " Hostname is : $HOSTNAME "
+echo "Successfully Created User"
+echo "User Name: ${USERNAME}"
+echo "Initial Password: ${PASSWORD}"
+echo "Hostname is: $HOSTNAME"
